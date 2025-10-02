27f74867a840e23db5a2c036cf27a025029caf3b
davecalleja-merkle <90806540+davecalleja-merkle@users.noreply.github.com>
2025-10-02 13:59:47 +0800

    Add next.config.js via Plop.js design system scaffolding

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    // Add any experimental features here
+  },
+  // Add any other Next.js configuration here
+}
+
+module.exports = nextConfig
